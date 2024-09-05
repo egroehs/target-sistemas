adbd49742d3455f136b8e84127d62755492b5637
egroehs <eduardagroehs@gmail.com>
2024-09-05 10:18:54 -0700

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,27 @@
+
+function fibonacci(numero) {
+    
+    let num1 = 0, num2 = 1;
+
+    if(numero < 0) {
+    console.log("Numero invalido");
+    }
+
+    if (numero === 0 || numero === 1) {
+        console.log(`O número ${numero} pertence à sequência de Fibonacci.`);
+    }
+
+    while (num2 < numero) {
+      let proximoNum = num1 + num2;
+      num1 = num2;
+      num2 = proximoNum;
+    }
+
+    if (num2 === numero) {
+      console.log(`O número ${numero} pertence a sequencia de Fibonacci`);
+    } else {
+      console.log(`O número ${numero} não pertence a sequencia de Fibonacci`);
+    }
+}
+
+fibonacci(45);
